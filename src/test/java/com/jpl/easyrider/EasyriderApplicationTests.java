a008a83b844ce80a0a15ac1aa262cff1804391a1
januszpilarski <Mango_1406>
2019-07-14 18:09:20 +0200

    Initial commit

---
+++ src/test/java/com/jpl/easyrider/EasyriderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpl.easyrider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyriderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
